b98ca35189049e53f94f43d286094e0cc982d996
Diogo pereira <dsilveri@c1r20s1.42lisboa.com>
2022-05-30 17:46:39 +0100

    update parser.c

--- minishell/srcs/parser.c
+++ minishell/srcs/parser.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dsilveri <dsilveri@student.42lisboa.com    +#+  +:+       +#+        */
+/*   By: dsilveri <dsilveri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 10:57:10 by dsilveri          #+#    #+#             */
-/*   Updated: 2022/05/29 18:40:43 by dsilveri         ###   ########.fr       */
+/*   Updated: 2022/05/30 17:44:32 by dsilveri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,24 @@
 
 static char *get_redir(char *src, int *index);
 static char *get_word(char *src, int *index);
+static char *get_quoted_word(char *src, int *index);
 
 char *get_next_token(char *src)
 {
 	static int	i = 0;
 	char		*dest;
 	
+	dest = NULL;
 	while (src[i] != '\0')
 	{
 		if (src[i] == '<' || src[i] == '>')
-		{
 			dest = get_redir(src, &i);
-			return (dest);
-		}
+		else if (src[i] == '\'' || src[i] == '\"')
+			dest = get_quoted_word(src, &i);
 		else if (!ft_strchr(WITHE_SPACES, src[i]))
-		{
 			dest = get_word(src, &i);
+		if(dest)
 			return (dest);
-		}
 		i++;
 	}
 	i = 0;
@@ -56,7 +56,6 @@ static char *get_redir(char *src, int *index)
 	return (dest);
 }
 
-
 static char *get_word(char *src, int *index)
 {
 	char	*dest;
@@ -73,3 +72,22 @@ static char *get_word(char *src, int *index)
 	*index = i;
 	return (dest);
 }
+
+static char *get_quoted_word(char *src, int *index)
+{
+	char	*dest;
+	int		i;
+
+	i = *index;
+	while(src[i])
+	{
+		if (i > *index && ft_strchr("\'\"", src[i]))
+			break;
+		i++;
+	}
+	i++;
+ 	dest = ft_substr(src, *index, i - (*index));
+	*index = i;
+	return (dest);
+}
+

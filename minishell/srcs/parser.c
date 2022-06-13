1780ccdc1fde9eb1b014a2254a4b63c512037426
Diogo Pereira <diogo.pereira.ee@gmail.com>
2022-06-13 16:53:54 +0100

    refactored parser.c and tree.c

--- minishell/srcs/parser.c
+++ minishell/srcs/parser.c
@@ -6,19 +6,19 @@
 /*   By: dsilveri <dsilveri@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 19:19:12 by dsilveri          #+#    #+#             */
-/*   Updated: 2022/06/10 17:59:15 by dsilveri         ###   ########.fr       */
+/*   Updated: 2022/06/13 16:52:20 by dsilveri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 static void	create_ast(t_node **tree, char *token);
-int get_node_id(char *token);
-int get_token_id(char *token);
-t_node *get_node_to_update(t_node *tree);
-t_node *update_node(t_node *node, char *token);
+int			get_node_id(char *token);
+int			get_token_id(char *token);
+t_node		*get_node_to_update(t_node *tree);
+t_node		*update_node(t_node *node, char *token);
 
-void print_node(t_node *node)
+void	print_node(t_node *node)
 {
 	if (!node)
 	{
@@ -35,12 +35,11 @@ void print_node(t_node *node)
 	printf("##########################\n");
 }
 
-
 t_node	*parser(char *src)
 {
 	char	*token;
 	t_node	*tree;
-	t_node *tree_b;
+	t_node	*tree_b;
 
 	tree = NULL;
 	token = get_next_token(src);
@@ -49,26 +48,7 @@ t_node	*parser(char *src)
 		create_ast(&tree, token);
 		token = get_next_token(src);
 	}
-
-	
-	// Apenas para testes print do brunch da tree
-	/*int i = 0;
-	if (tree)
-	{
-		tree_b = tree;
-		while (tree_b)
-		{
-			print_node(tree_b);
-			//if (i == 0)
-			//	tree_b = tree_b->rigth;
-			//else 
-				tree_b = tree_b->left;
-			i++;
-		}
-	}*/
-
 	print2D(tree);
-
 	return (0);
 }
 
@@ -76,90 +56,46 @@ static void	create_ast(t_node **tree, char *token)
 {
 	int		id;
 	t_node	*node;
-	
+
 	id = get_token_id(token);
 	if (id == ID_WORD)
 	{
 		node = get_node_to_update(*tree);
 		if (node)
-			update_node(node, token); //vai fazer update de comandos também regra ainda não criada
+			update_node(node, token);
 		else
 			add_new_node(tree, update_node(create_node(ID_CMD), token));
 	}
-	else 
+	else
 		add_new_node(tree, create_node(id));
 }
 
-/*
-	Esta função dever ser melhorada e faze-la de forma mais simple
-*/
-t_node *get_node_to_update(t_node *tree)
+t_node	*get_node_to_update(t_node *tree)
 {
-	t_node *node;
+	t_node	*node;
+	t_node	*next;
 
 	if (!tree)
 		return (NULL);
-	if (tree->id != ID_PIPE) // Navega pela esquerda
-	{
+	if (is_node_pipe(*tree))
+		node = tree->rigth;
+	else
 		node = tree;
-		while (node)
-		{
-			if (is_node_redir(*node))
-			{
-				if (!(node->data))
-					return (node);
-				else
-					break;
-			}
-			node = node->left;
-		}
-
-
-		node = tree;
-		while (node)
-		{
-			if (is_node_cmd(*node))
-				return (node);
-			node = node->left;
-		}
-	}
-	else //navega pela direita
-	{
-		node = tree;
-		if (node->rigth == NULL)
-			return (NULL);
-		else if (node->rigth && is_node_redir(*(node->rigth)) && node->rigth->data == NULL)
-			return(node->rigth);
-		else if (node->rigth->left && is_node_redir(*(node->rigth->left)) && node->rigth->left->data == NULL)
-			return(node->rigth->left);
-		else if (node->rigth && is_node_cmd(*(node->rigth)))
-			return(node->rigth);
-	}
+	if (!node)
+		return (NULL);
+	next = node->left;
+	if ((is_node_redir(*node) && !(node->data)))
+		return (node);
+	else if (next && is_node_redir(*next) && !(next->data))
+		return (next);
+	else if (is_node_cmd(*node))
+		return (node);
 	return (NULL);
 }
 
-// apenas resolve strings no futuro terá de ser array de string
-/*t_node *update_node1(t_node *node, char *token)
+int	get_size_string_array(char **str)
 {
-	int size;
-
-	if (!node)
-		return (NULL);
-	if (!(node->data))
-	{
-		size = ft_strlen(token);
-		node->data = ft_calloc(size + 1, sizeof(char));
-		if (!(node->data))
-			return (NULL);
-		ft_strlcpy(node->data, token, size + 1);
-	}
-	return (node);
-}*/
-
-
-int get_size_string_array(char **str)
-{
-	int i;
+	int	i;
 
 	if (!str)
 		return (0);
@@ -169,58 +105,49 @@ int get_size_string_array(char **str)
 	return (i);
 }
 
-char **update_string_array(char **str, char *token)
+char	**update_string_array(char **str, char *token)
 {
-	int arr_size;
-	int token_size;
-	char **new_arr;
-	int i;
+	char	**new_arr;
+	char	**buff;
+	int		arr_size;
+	int		i;
 
-	
-	arr_size = get_size_string_array(str);
-	new_arr = malloc((arr_size + 2) * sizeof(char**));
-	i = 0;
-	while (str[i])
+	if (!str)
 	{
-		new_arr[i] = str[i]; 
-		i++;
+		new_arr = malloc(2 * sizeof(char *));
+		new_arr[0] = ft_strdup(token);
+		new_arr[1] = NULL;
 	}
-	token_size = ft_strlen(token);
-	new_arr[i] = ft_calloc((token_size + 1), sizeof(char));
-	ft_strlcpy(new_arr[i], token, token_size + 1);
-	free(str);
+	else
+	{
+		arr_size = get_size_string_array(str);
+		new_arr = malloc((arr_size + 2) * sizeof(char *));
+		i = -1;
+		while (str[++i])
+			new_arr[i] = str[i];
+		new_arr[i] = ft_strdup(token);
+		new_arr[i + 1] = 0;
+		free(str);
+	}
 	return (new_arr);
 }
 
-
-t_node *update_node(t_node *node, char *token)
+t_node	*update_node(t_node *node, char *token)
 {
-	int size;
+	int	size;
 
 	if (!node || !token)
 		return (NULL);
-
-	if(!(node->data))
-	{
-		size = ft_strlen(token);
-		node->data = malloc(2 * sizeof(char**));
-		node->data[0] = ft_calloc(size + 1, sizeof(char));
-		node->data[1] = NULL;
-		ft_strlcpy(node->data[0], token, size + 1);
-	}
-	else 
-		node->data = update_string_array(node->data, token);
+	node->data = update_string_array(node->data, token);
 	return (node);
 }
 
-
-
-int get_token_id(char *token)
+int	get_token_id(char *token)
 {
-	int size;
+	int	size;
 
 	size = ft_strlen(token);
-	if(size == 1)
+	if (size == 1)
 	{
 		if (token[0] == '<')
 			return (ID_IN_REDIR);
@@ -229,7 +156,7 @@ int get_token_id(char *token)
 		else if (token[0] == '|')
 			return (ID_PIPE);
 	}
-	if(size == 2)
+	if (size == 2)
 	{
 		if (token[0] == '<' && token[1] == '<')
 			return (ID_IN_HERDOC);
@@ -239,21 +166,21 @@ int get_token_id(char *token)
 	return (ID_WORD);
 }
 
-int is_node_redir(t_node node)
+int	is_node_redir(t_node node)
 {
 	if (node.id >= ID_IN_REDIR && node.id <= ID_OUT_HERDOC)
 		return (1);
 	return (0);
 }
 
-int is_node_pipe(t_node node)
+int	is_node_pipe(t_node node)
 {
-	if (node.id >= ID_PIPE)
+	if (node.id == ID_PIPE)
 		return (1);
 	return (0);
 }
 
-int is_node_cmd(t_node node)
+int	is_node_cmd(t_node node)
 {
 	if (node.id >= ID_CMD)
 		return (1);
--- minishell/srcs/tree.c
+++ minishell/srcs/tree.c
@@ -6,21 +6,25 @@
 /*   By: dsilveri <dsilveri@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 19:19:06 by dsilveri          #+#    #+#             */
-/*   Updated: 2022/06/09 12:46:54 by dsilveri         ###   ########.fr       */
+/*   Updated: 2022/06/13 14:16:05 by dsilveri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+//cat <in teste  >out | ls <in2 >out2 -la | <<in3 >>out3 grep "jun"
+
 #include "minishell.h"
 
-void add_new_redir(t_node **tree, t_node *node);
-void add_new_cmd(t_node **tree, t_node *node);
-void add_new_pipe(t_node **tree, t_node *node);
+#define LEFT 	0
+#define RIGHT 	1
 
-void add_node_after_left(t_node *node, t_node *new_node);
+void	add_new_redir(t_node **tree, t_node *node);
+void	add_new_cmd(t_node **tree, t_node *node);
+void	add_new_pipe(t_node **tree, t_node *node);
+void	add_node_after_left(t_node *node, t_node *new_node);
 
-t_node  *create_node(int id)
+t_node	*create_node(int id)
 {
-	t_node *node;
+	t_node	*node;
 
 	node = malloc(sizeof(t_node));
 	if (!node)
@@ -33,40 +37,35 @@ t_node  *create_node(int id)
 	return (node);
 }
 
-void add_node_after_left(t_node *node, t_node *new_node)
+void	add_node_after(t_node *node, t_node *new_node, int dir)
 {
 	if (!node || !new_node)
 		return ;
-	new_node->prev = node;
-	if ((node->left))
+	else if (dir == LEFT && (node->left))
 	{
 		new_node->left = node->left;
 		node->left->prev = new_node;
 	}
-	node->left = new_node;
-}
-
-void add_node_after_right(t_node *node, t_node *new_node)
-{
-	if (!node || !new_node)
-		return ;
-	if(node->rigth)
+	else if (dir == RIGHT && (node->rigth))
 	{
 		new_node->left = node->rigth;
 		node->rigth->prev = new_node;
 	}
+	if (dir == LEFT)
+		node->left = new_node;
+	else if (dir == RIGHT)
+		node->rigth = new_node;
 	new_node->prev = node;
-	node->rigth = new_node;
 }
 
-void add_node_on_top(t_node **tree, t_node *node)
+void	add_node_on_top(t_node **tree, t_node *node)
 {
-	t_node *first;
+	t_node	*first;
 
 	first = *tree;
 	if (!first)
 		first = node;
-	else 
+	else
 	{
 		node->left = first;
 		first->prev = node;
@@ -75,17 +74,17 @@ void add_node_on_top(t_node **tree, t_node *node)
 	*tree = first;
 }
 
-void add_new_node(t_node **tree, t_node *node)
+void	add_new_node(t_node **tree, t_node *node)
 {
 	if (is_node_redir(*node))
 		add_new_redir(tree, node);
 	else if (is_node_cmd(*node))
 		add_new_cmd(tree, node);
 	else if (is_node_pipe(*node))
-		add_new_pipe(tree, node);
+		add_node_on_top(tree, node);
 }
 
-void add_new_redir(t_node **tree, t_node *node)
+void	add_new_redir(t_node **tree, t_node *node)
 {
 	t_node	*first;
 	t_node	*next;	
@@ -96,37 +95,24 @@ void add_new_redir(t_node **tree, t_node *node)
 	if (!first || is_node_redir(*first))
 		add_node_on_top(tree, node);
 	else if (is_node_cmd(*first))
-		add_node_after_left(first, node);
+		add_node_after(first, node, LEFT);
 	else if (is_node_pipe(*first))
 	{
 		next = first->rigth;
-		if (!next || is_node_redir(*next))	
-			add_node_after_right(first, node);
+		if (!next || is_node_redir(*next))
+			add_node_after(first, node, RIGHT);
 		else
-			add_node_after_left(next, node);
+			add_node_after(next, node, LEFT);
 	}
 }
 
-void add_new_cmd(t_node **tree, t_node *node)
+void	add_new_cmd(t_node **tree, t_node *node)
 {
-	t_node *first;
-	
+	t_node	*first;
+
 	first = *tree;
 	if (!first || is_node_redir(*first))
 		add_node_on_top(tree, node);
 	else if (is_node_pipe(*first))
-		add_node_after_right(first, node);
-}
-
-void add_new_pipe(t_node **tree, t_node *node)
-{
-	t_node *first;
-	
-	first = *tree;
-	if (!first)
-		printf("Não pode e erro de sintax/parser. O pipe não pode ser o primeiro\n");
-	else if (first->id != ID_CMD && first->id != ID_PIPE)
-		printf("Não pode e erro de sintax/parser. O pipe não pode ser o primeiro\n");
-	else
-		add_node_on_top(tree, node);
+		add_node_after(first, node, RIGHT);
 }

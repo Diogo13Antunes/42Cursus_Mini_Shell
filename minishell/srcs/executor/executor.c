c5d75b27e0bee89b440786d78298e8fbc25175a0
Diogo Pereira <diogo.pereira.ee@gmail.com>
2022-07-15 11:58:18 +0100

    fixed wait_cmds.c to send the correct exit code

--- minishell/srcs/executor/executor.c
+++ minishell/srcs/executor/executor.c
@@ -12,15 +12,16 @@
 
 #include "minishell.h"
 
-static void	tree_inorder_traversal(t_node *root, t_env *env);
+static void	exec_cmd_forks(t_node *root, t_env *env, int *l_pid);
 static void	exec(t_node *tree, t_env *env);
 static void	run_cmd(t_node node, t_env *env);
-static int	wait_cmds(int n_cmds);
+static int	wait_cmds(int n_cmds, int l_pid);
 
 int	execution(t_node *tree, t_env *env)
 {
 	char	**cmd;
 	int		exit_code;
+	int		l_pid;
 
 	if (is_builtin_without_pipe(tree))
 	{
@@ -32,31 +33,33 @@ int	execution(t_node *tree, t_env *env)
 	{
 		open_pipes(tree);
 		hdoc_exec(tree);
-		tree_inorder_traversal(tree, env);
+		exec_cmd_forks(tree, env, &l_pid);
 		close_pipes(tree);
 		close_hdoc(tree);
-		exit_code = wait_cmds(get_num_cmds(tree));
+		exit_code = wait_cmds(get_num_cmds(tree), l_pid);
 	}
 	return (exit_code);
 }
 
-static void	tree_inorder_traversal(t_node *root, t_env *env)
+static void	exec_cmd_forks(t_node *root, t_env *env, int *l_pid)
 {
 	int	pid;
 
 	if (!root)
 		return ;
-	tree_inorder_traversal(root->left, env);
+	exec_cmd_forks(root->left, env, l_pid);
 	if (root->left == NULL)
 	{
 		pid = fork();
-		if (!pid)
+		if (pid)
+			*l_pid = pid;
+		if (!(pid))
 		{
 			exec(root, env);
 			exit(0);
 		}
 	}
-	tree_inorder_traversal(root->rigth, env);
+	exec_cmd_forks(root->rigth, env, l_pid);
 }
 
 static void	exec(t_node *tree, t_env *env)
@@ -92,16 +95,20 @@ static void	run_cmd(t_node node, t_env *env)
 	}
 }
 
-static int	wait_cmds(int n_cmds)
+static int	wait_cmds(int n_cmds, int l_pid)
 {
 	int	status;
 	int	exit_code;
 
+	if (waitpid(l_pid, &status, 0))
+	{
+		if (WIFEXITED(status))
+			exit_code = WEXITSTATUS(status);
+	}
+	n_cmds--;
 	while (n_cmds)
 	{
 		wait(&status);
-		if (WIFEXITED(status))
-			exit_code = WEXITSTATUS(status);
 		n_cmds--;
 	}
 	return (exit_code);

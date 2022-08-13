4c3d4cd8a5c1ffd3921c2133ece8b1be251013b8
Diogo Pereira <diogo.pereira.ee@gmail.com>
2022-08-13 18:01:57 +0100

    created exit_status() function to save status code inside of the CTRL-C handler function

--- minishell/srcs/minishell.c
+++ minishell/srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: dsilveri <dsilveri@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/29 18:42:18 by dsilveri          #+#    #+#             */
-/*   Updated: 2022/08/13 16:18:29 by dsilveri         ###   ########.fr       */
+/*   Updated: 2022/08/13 17:52:09 by dsilveri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,27 @@
 #include <signal.h> 
 
 static void test_parser(char *src);
+
+
+// criar um modulo que guarda o exit_status numa variável estática
+// save_exit_status(int code)
+// clean_exit_status(void)
+// int get_exit_status()
+int exit_status(int code, int op)
+{
+    static int status_code = 0;
+    int buff;
+
+    if (op == 1)
+        status_code = code;
+    else if (op == 2)
+    {
+        buff = status_code; 
+        status_code = 0;
+        return (buff);
+    }
+    return (status_code);
+}
 
 void get_new_prompt(int signum)
 {
@@ -23,6 +44,7 @@ void get_new_prompt(int signum)
         ft_putstr_fd("\n", STDOUT_FILENO);
         rl_on_new_line();
         rl_redisplay();
+        exit_status(EXIT_CTRLC_SIGNAL, 1);
     }
 }
 
@@ -43,6 +65,8 @@ int main (int argc, char **argv, char **env)
         config_signal(get_new_prompt);
 	    prompt = get_prompt_str(env_lst);
         str = readline(prompt);
+        if (exit_status(0, 0))
+           exit_code = exit_status(0, 2);
         config_signal(SIG_IGN);
         //signals_call(SG_IGN);
         if (str && str[0])

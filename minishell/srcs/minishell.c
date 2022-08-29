6d9b09212a4613f410c7ffb02a334c4560ab44f1
Diogo andre Candeias antunes <dcandeia@c2r9s7.42lisboa.com>
2022-08-29 17:33:47 +0100

    removed comments from minishell.c

--- minishell/srcs/minishell.c
+++ minishell/srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: dcandeia <dcandeia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/29 18:42:18 by dsilveri          #+#    #+#             */
-/*   Updated: 2022/08/29 09:38:42 by dcandeia         ###   ########.fr       */
+/*   Updated: 2022/08/29 17:32:19 by dcandeia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,17 +36,17 @@ int	main(int argc, char **argv, char **env)
 		str = readline(prompt);
 		if (!str)
 		{
-			free(prompt);
+			free_str(prompt);
 			builtin_exit();
 		}
 		set_signal(SIGINT, SIG_IGN);
 		if (str && str[0])
 			add_history(str);
-		free(prompt);
+		free_str(prompt);
 		exit_code = get_exit_status();
 		tree = parser(str, env_lst, &exit_code);
 		save_alloc_mem(tree, TYPE_TREE);
-		free(str);
+		free_str(str);
 		if (tree)
 		{
 			clear_exit_status();
@@ -57,65 +57,5 @@ int	main(int argc, char **argv, char **env)
 		}
 	}
 	free_all_alloc_mem();
-	/*
-		teste para verificar se a tree é corretamente limpa
-		tree = parser("<in ls -la | cat | wc -l >out", NULL, exit_code);
-		print2D(tree);
-		free_tree(tree);
-	*/
 	return (0);
 }
-
-/* teste free memory
-    --leak-check=full
-    --leak-check=full --show-leak-kinds=all
-*/
-/*
-int main (int argc, char **argv, char **env)
-{
-    char *str;
-    t_node *tree;
-	t_env	*env_lst;
-	char *prompt;
-    int exit_code;
-    t_exit_status ex;
-
-    init_alloc_mem();
-    set_signal(SIGQUIT, SIG_IGN);
-    exit_code = 0;
-	env_lst = get_env_list(env);
-    save_alloc_mem(env_lst, TYPE_ENV);
-
-        set_signal(SIGINT, new_prompt_handler);
-	    prompt = get_prompt_str(env_lst);
-        //str = readline(prompt);
-        str = ft_calloc(sizeof(char), 100);
-        ft_strcat(str, "exit");
-        if (!str)
-            builtin_exit();
-        set_signal(SIGINT, SIG_IGN);
-        //if (str && str[0])
-            //add_history(str);
-		free(prompt);
-        exit_code = get_exit_status();
-        tree = parser(str, env_lst, &exit_code);
-        save_alloc_mem(tree, TYPE_TREE);
-        free(str);
-        if (tree)
-        {
-            clear_exit_status();
-            execution(tree, env_lst);
-            exit_code = get_exit_status();
-            free_tree(tree);
-            save_alloc_mem(NULL, TYPE_TREE);
-        }
-    free_all_alloc_mem();
-    
-    //teste para verificar se a tree é corretamente limpa 
-    //tree = parser("<in ls -la | cat | wc -l >out", NULL, exit_code);
-    //print2D(tree);
-    //free_tree(tree);
-
-    return (0);
-}
-*/
